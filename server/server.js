a131b2538a172c2918259ba2e0c019acaa5065c3
issra <>
2020-09-10 17:48:25 +0300

    upload

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.get('/', function(req, res) {
+    res.send('Hello form server');
+});
+
+app.post('/enroll', function(req, res) {
+    console.log(req.body);
+    res.status(401).send({ message: 'Date received' });
+});
+
+app.listen(PORT, function() {
+    console.log('Server running on localhost: ' + PORT);
+});
